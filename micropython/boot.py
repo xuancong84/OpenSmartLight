808d61fe39ea2528e94e0ea89f588b5fe7dccb65
Wang Xuancong <xuancong84@gmail.com>
2023-06-22 00:08:55 +0800

    temp2

--- micropython/boot.py
+++ micropython/boot.py
@@ -1,9 +1,11 @@
-import uos, machine, gc, network
+import uos, machine, gc, network, socket, select
 
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(False)
 ap_if = network.WLAN(network.AP_IF)
 ap_if.active(False)
+
+gc.collect()
 
 def connect_wifi():
 	if sta_if.active():
@@ -24,16 +26,53 @@ def create_hotspot():
 	ap_if.config(ssid='ESP-AP', authmode=network.AUTH_OPEN)
 	print(ap_if.ifconfig())
 
-def create_webserver():	
-	import socket
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind(('', 80))
-	s.listen(5)
 
-	while True:
-		conn, addr = s.accept()
-		print('Got a connection from %s' % str(addr))
-		request = conn.recv(1024)
-		print('Content = %s' % str(request))
-		conn.send("""<html><head><meta name="viewport" content="width=device-width, initial-scale=1"</head><body><h1>Hello, World!</h1></body></html>""")
-		conn.close()
+import uasyncio as asyncio
+from microdot_asyncio import Microdot
+
+app = Microdot()
+N=0
+@app.route('/')
+async def hello(request):
+	global N
+	N += 1
+	return f'Hello world = {N}'
+
+def start_server():
+	print('Starting microdot app')
+	try:
+		app.run(port=80)
+	except:
+		app.shutdown()
+
+def start():
+	create_hotspot()
+	import captiveDNS
+	cd=captiveDNS.CaptiveDNS('192.168.4.1')
+	start_server()
+
+# class WebServer:
+# 	def __init__(self, host='0.0.0.0', captivePortal=False, port=80, max_conn=8):
+# 		self.sock_web = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 		self.sock_web.bind(('', port))
+# 		self.sock_web.listen(max_conn)
+# 		self.poll = select.poll()
+# 		self.poll.register(self.sock_web, select.POLLIN)
+
+# 		if captivePortal:
+# 			self.sock_dns = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 			self.sock_dns.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# 			self.sock_dns.bind((host, port))
+# 			self.poll.register(self.sock_dns, select.POLLIN)
+# 		else:
+# 			self.sock_dns = None
+
+# 	def run(self, dur=None):
+# 		while True:
+# 			self.poll.poll(dur)
+# 			conn, addr = self.sock_web.accept()
+# 			print('Got a connection from %s' % str(addr))
+# 			request = conn.recv(1024)
+# 			print('Content = %s' % str(request))
+# 			conn.send("""<html><head><meta name="viewport" content="width=device-width, initial-scale=1"</head><body><h1>Hello, World!</h1></body></html>""")
+# 			conn.close()
