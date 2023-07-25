169b3efbaed730504926f7509fb4bb4c091c3c57
Wang Xuancong <xuancong84@gmail.com>
2023-07-25 09:04:04 +0800

    added ESP32-C3

---
+++ micropython/esp32c3/boot.py
@@ -0,0 +1,25 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import os, machine, gc, network, time
+#os.dupterm(None, 1) # disable REPL on UART(0)
+
+sta_if = network.WLAN(network.STA_IF)
+ap_if = network.WLAN(network.AP_IF)
+sta_if.active(False)
+ap_if.active(False)
+
+if True:
+	sta_if.active(True)
+	import webrepl
+	webrepl.start()
+
+gc.collect()
+
+mains = [f for f in os.listdir() if f.startswith('main')]
+
+if mains:
+	exec(f'from {mains[0].split(".")[0]} import *')
+	gc.collect()
+	if machine.Pin(9, machine.Pin.IN, machine.Pin.PULL_UP)():
+		run()
