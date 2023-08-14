d7a67d5cabfce8f1e1d5d83326d3a860910c0c24
Wang Xuancong <xuancong84@gmail.com>
2023-08-14 17:49:01 +0800

    support -ve pin number, inverted signal

--- micropython/boot.py
+++ micropython/boot.py
@@ -9,20 +9,20 @@ ap_if = network.WLAN(network.AP_IF)
 sta_if.active(False)
 ap_if.active(False)
 
-if True:
-	sta_if.active(True)
-	import webrepl
-	webrepl.start()
-
 gc.collect()
 
 mains = [f for f in os.listdir() if f.startswith('main')]
 
-if mains:
-	exec(f'from {mains[0].split(".")[0]} import *')
-	gc.collect()
-	p16 = machine.Pin(16, machine.Pin.OUT)
-	p16(1)
-	time.sleep(0.1)
-	if machine.Pin(16, machine.Pin.IN)():
-		run()
+try:
+	if mains:
+		exec(f'from {mains[0].split(".")[0]} import *')
+		gc.collect()
+		p16 = machine.Pin(16, machine.Pin.OUT)
+		p16(1)
+		time.sleep(0.1)
+		if machine.Pin(16, machine.Pin.IN)():
+			run()
+except:
+	ap_if.active(True)
+	import webrepl
+	webrepl.start()
