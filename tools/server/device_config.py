a86b0b59a608aa826077ec42344de2d0e8e0d7e0
Wang Xuancong <xuancong84@gmail.com>
2024-02-23 23:15:19 +0800

    KTV fully working

--- tools/server/device_config.py
+++ tools/server/device_config.py
@@ -2,6 +2,7 @@
 # all fields must be set, if absent put ''
 #KTV_SPEAKER='EC:52:32:4F:07:56'
 KTV_SPEAKER='10:3C:88:17:20:78'
+KTV_SCREEN='livingTV'
 MP3_SPEAKER='54:B7:E5:9E:F4:14'
 MP4_SPEAKER=['hdmi', 'audio.stereo']
 MIC_RECORDER='usb'
